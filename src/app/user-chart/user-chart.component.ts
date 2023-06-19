4126acc48b1c987cdd6b6b81fd1de5649b47d161
pkieber <113725311+pkieber@users.noreply.github.com>
2023-06-19 20:24:10 +0200

    update color

--- src/app/user-chart/user-chart.component.ts
+++ src/app/user-chart/user-chart.component.ts
@@ -88,11 +88,11 @@ export class UserChartComponent implements OnInit {
             this.numberOfProduction
           ],
           backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 99, 132, 0.7)',
+            'rgba(54, 162, 235, 0.7)',
+            'rgba(255, 206, 86, 0.7)',
+            'rgba(75, 192, 192, 0.7)',
+            'rgba(153, 102, 255, 0.7)',
           ],
           borderColor: [
             'rgba(255, 99, 132, 1)',
