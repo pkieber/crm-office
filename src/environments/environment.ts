b9eecfe95f4e910748d7d8ca6afcaec36488fbca
pkieber <113725311+pkieber@users.noreply.github.com>
2023-07-26 22:58:36 +0200

    update files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,7 @@
+import { defaultEnvironment } from "./environment.default";
+
 export const environment = {
-  firebase: {
-    projectId: 'simple-crm-9575e',
-    appId: '1:384466249828:web:4729e98e3bd63ee07a9771',
-    storageBucket: 'simple-crm-9575e.appspot.com',
-    apiKey: 'AIzaSyDiyK_7sHXein6OZJKgNbojDbLd-HeC4ss',
-    authDomain: 'simple-crm-9575e.firebaseapp.com',
-    messagingSenderId: '384466249828',
-    measurementId: 'G-3XPFKZF400',
-  },
+  ...defaultEnvironment,
+
+  production: false
 };
